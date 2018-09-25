030a6956253acb1da5ba9ffc761b8240c787d188
Tim Short <timothyjames.short@gmail.com>
2018-09-25 16:52:48 -0400

    Selenium Day 2 assertions lab

--- basics/LoginDDT.java
+++ basics/LoginDDT.java
@@ -1,5 +1,6 @@
 package basics;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import utilities.DriverFactory;
@@ -11,6 +12,9 @@ public class LoginDDT {
 		String[] passwords = { "password", "password", "Password1@" };
 		String[] expectedOutcome = { "pass", "fail", "pass" };
 		String[] browserType = { "chrome", "IE", "Firefox" };
+		String url = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
+		String successMessage = "Logged in successfully";
+		String failedMessage = "Invalid user name";
 		
 		// For loop to iterate through records
 		for (int i=0; i<emails.length; i++) {
@@ -19,6 +23,34 @@ public class LoginDDT {
 			System.out.println("EMAIL: " + emails[i]);
 			System.out.println("PASSWORD: " + passwords[i]);
 			System.out.println("EXPECTATION: " + expectedOutcome[i]);
+			
+			// Initialize expected message
+			String message = successMessage;
+			if (expectedOutcome[i].equals("fail")) {
+				message = failedMessage;
+			}
+			System.out.println(" MESSAGE: " + message);
+			
+			// Launch browser
+			WebDriver driver = DriverFactory.get(browserType[i]);
+			driver.get(url);
+			
+			// Fill out form
+			driver.findElement(By.id("MainContent_txtUserName")).sendKeys(emails[i]);
+			driver.findElement(By.id("MainContent_txtPassword")).sendKeys(passwords[i]);
+			driver.findElement(By.id("MainContent_btnLogin")).click();
+			
+			// Verification
+			if (driver.getPageSource().contains(message)) {
+				System.out.println("TEST PASSED!");
+			}
+			else {
+				System.out.println("TEST FAILED!");
+			}
+			System.out.println("**********");
+			
+			// Close browser
+			driver.close();
 		}
 		
 	}
