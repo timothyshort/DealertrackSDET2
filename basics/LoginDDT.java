3a5ded14e0e6ecfd2420d0cc5b27b1d3e3ae4c48
Tim Short <timothyjames.short@gmail.com>
2018-09-25 16:17:16 -0400

    Selenium outline for data-driven testing lab

--- basics/LoginDDT.java
+++ basics/LoginDDT.java
@@ -7,8 +7,20 @@ import utilities.DriverFactory;
 public class LoginDDT {
 
 	public static void main(String[] args) {
-		// Fetch browser from framework
-		WebDriver driver = DriverFactory.get("ie");
+		String[] emails = { "tim@testemail.com", "bademail@mail.com", "montyscorp@gmail.com" };
+		String[] passwords = { "password", "password", "Password1@" };
+		String[] expectedOutcome = { "pass", "fail", "pass" };
+		String[] browserType = { "chrome", "IE", "Firefox" };
+		
+		// For loop to iterate through records
+		for (int i=0; i<emails.length; i++) {
+			System.out.println("\nTEST STARTING");
+			System.out.println("Browser: " + browserType[i]);
+			System.out.println("EMAIL: " + emails[i]);
+			System.out.println("PASSWORD: " + passwords[i]);
+			System.out.println("EXPECTATION: " + expectedOutcome[i]);
+		}
+		
 	}
 
 }
